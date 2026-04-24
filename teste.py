1cb1cc9f8089ec24dab138cb613a44465460e4a0
Diego <diego@example.com>
2026-04-24 13:05:13 -0300

    add initial print statement in teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,46 @@
-print("oi, meu nome é Diego")
+print("hello, diego")
+raiz = 100** (1/2)
+print (raiz)
+
+raiz = 36** 1.5
+print (raiz)
+
+x= 900
+raiz=x**0.5
+print (raiz)
+
+a=10
+b=20
+c=30
+media = (a+b+c)/3
+print ("o valor da media é:",media)
+
+a=1
+b=5
+c=4
+
+delta=b**2-4*a*c
+print ("o valor do delta é:",delta)
+
+print("o valor da raiz de delta é:",delta,"Considerando a=", a,"b=",b,"c=",c)
+
+print("o valor de delta é {}considerando a={},b={},c={}".format(delta, a, b, c))
+
+z=10
+print(type(z))
+
+w=10/2
+print(type(w))
+
+nome = input("por favor, digite seu nome")
+print ("seu nome é," , nome)
+
+nomeclatura = input("por favor, digite seu nome")
+print ("seu nome é," , nomeclatura)
+x=int (input ("digite um valor"))
+y=int (input ("digite outro valor"))
+print (x+y)
+
+x=int (input ("digite um valor"))
+y=int (input ("digite outro valor"))
+print (x+y)
